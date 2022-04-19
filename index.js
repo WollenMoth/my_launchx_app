927e6341196216ac264e56faec5a0c639365112e
André Díaz <andre290800@gmail.com>
2022-04-18 23:23:07 -0500

    Creación de una clase y modularización

---
+++ index.js
@@ -0,0 +1,10 @@
+const MissionCommander = require("./app/missionCommander");
+
+const carlo = new MissionCommander("Carlo");
+console.log(carlo.name);
+
+const fernanda = new MissionCommander("Fernanda");
+console.log(fernanda.name);
+
+const rodrigo = new MissionCommander("Rodrigo");
+console.log(rodrigo.name);
